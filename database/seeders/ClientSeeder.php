8c8635a178a9fa48a2079a2dee89ee59325c28eb
FRANZSAIJEM <mikoremixes@gmail.com>
2023-07-14 07:17:32 +0800

    email

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -14,5 +14,32 @@ class ClientSeeder extends Seeder
     public function run(): void
     {
         Client::factory(30)->create();
+        $cl = [
+            [
+                'last_name' => 'Mejias',
+                'first_name' => 'Francis Michael',
+                'middle_name' => 'Cabrera',
+                'address' => 'Purok 2. Cagayan, Inabanga, Bohol',
+                'birth_date' => '2002-7-8',
+                'phone_number' => '09606765143',
+                'email' => 'mejiasfranzsaijem@gmail.com',
+                'initial_deposit' => 10000
+            ],
+            [
+                'last_name' => 'Cenita',
+                'first_name' => 'Zeus Gyro',
+                'middle_name' => 'Mejias',
+                'address' => ' Purok 4. Cagayan, Inabanga, Bohol',
+                'birth_date' => '1996-11-03',
+                'phone_number' => '09307665033',
+                'email' => 'zeusgyro@gmail.com',
+                'initial_deposit' => 10000
+            ]
+
+        ];
+
+        foreach($cl as $c) {
+            Client::create($c);
+        }
     }
 }
